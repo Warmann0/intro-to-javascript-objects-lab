c9665e3b329fd0a7e73e12142b464d2d0d413444
warmann0 <mnw@warmann.org>
2025-01-27 13:45:19 -0700

    Exercise 2 completed

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const game = {
+/// Exercise 1  
+
+/* const game = {
     party: [],
     gyms: [
       { location: "Pewter City", completed: false, difficulty: 1 },
@@ -17,3 +19,29 @@ const game = {
     ],
   }
   console.dir(pokemon, { maxArrayLength: null })
+*/ //  Exercise 1 completed
+
+/// Exercise 2  
+
+const game = {
+  party: [],
+  gyms: [
+    { location: "Pewter City", completed: false, difficulty: 1 },
+    { location: "Cerulean City", completed: false, difficulty: 2 },
+    { location: "Vermilion City", completed: false, difficulty: 3 },
+    { location: "Celadon City", completed: false, difficulty: 4 },
+    { location: "Fuchsia City", completed: false, difficulty: 5 },
+    { location: "Saffron City", completed: false, difficulty: 6 },
+    { location: "Cinnabar Island", completed: false, difficulty: 7 },
+    { location: "Viridian City", completed: false, difficulty: 8 },
+  ],
+  items: [
+    { name: "potion", quantity: 4 },
+    { name: "pokeball", quantity: 8 },
+    { name: "rare candy", quantity: 99 },
+  ],
+}
+console.log(pokemon[58])
+
+/// Exercise 3
+
