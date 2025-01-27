9239b22c4ec84dc3a4ceb1fb1d67fa3b4674689b
warmann0 <mnw@warmann.org>
2025-01-27 14:17:46 -0700

    Exercise 3 completed

--- app.js
+++ app.js
@@ -22,7 +22,7 @@
 */ //  Exercise 1 completed
 
 /// Exercise 2  
-
+/*
 const game = {
   party: [],
   gyms: [
@@ -43,5 +43,33 @@ const game = {
 }
 console.log(pokemon[58])
 
-/// Exercise 3
+/*
+Exercise 3
+1. Add a new property to the `game` object. Let's call it "difficulty".
+2. Choose a value for "difficulty" that you think fits the game. Ex: "Easy", "Med" or "Hard". How would you assign it?
 
+
+Solve Exercise 3 here:
+*/
+const game = {
+  party: [],
+  gyms: [
+    { location: "Pewter City", completed: false, difficulty: 1 },
+    { location: "Cerulean City", completed: false, difficulty: 2 },
+    { location: "Vermilion City", completed: false, difficulty: 3 },
+    { location: "Celadon City", completed: false, difficulty: 4 },
+    { location: "Fuchsia City", completed: false, difficulty: 5 },
+    { location: "Saffron City", completed: false, difficulty: 6 },
+    { location: "Cinnabar Island", completed: false, difficulty: 7 },
+    { location: "Viridian City", completed: false, difficulty: 8 },
+  ],
+  items: [
+    { name: "potion", quantity: 4 },
+    { name: "pokeball", quantity: 8 },
+    { name: "rare candy", quantity: 99 },
+  ],
+}
+game.difficulty = "Easy",
+
+
+console.log(game)
